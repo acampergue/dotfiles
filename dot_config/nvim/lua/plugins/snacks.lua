7bd3819461d95bb5aceca2293332d9a3e5168d20
Aurélien Campergue (GIP) <campergue@gipmbh.de>
2026-05-29 09:48:33 +0200

    Add .config/nvim/lua/plugins/snacks.lua

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            -- ignored = true, -- uncomment to also show gitignored files
+          },
+        },
+      },
+    },
+  },
+}
