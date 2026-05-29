9c5896ecdcc75bb9347ba67e446add60dd659129
Aurélien Campergue (GIP) <campergue@gipmbh.de>
2026-05-29 09:48:26 +0200

    Add .config/nvim/lua/plugins/numbertoggle.lua

---
+++ dot_config/nvim/lua/plugins/numbertoggle.lua
@@ -0,0 +1,8 @@
+return {
+  -- In a buffer with "hybrid" line numbers (:set number relativenumber),
+  -- numbertoggle switches to absolute line numbers (:set number norelativenumber)
+  -- automatically when relative numbers don't make sense.
+  --
+  -- https://jeffkreeftmeijer.com/vim-number/
+  { "jeffkreeftmeijer/vim-numbertoggle" },
+}
